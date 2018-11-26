b51b2a3988676e5680d9f15e5e58e9fcd699e5df
Luis-Marrero <44122162+Luis-Marrero@users.noreply.github.com>
2018-11-26 17:34:18 -0600

    Add files via upload

---
+++ Lab 7.ps1
@@ -0,0 +1,22 @@
+﻿$x = 0
+Set-location C:\
+while ($x -eq 0){
+    $y = read-host 'Do you want to exit? (y or n)'
+
+        if($y -eq 'y'){
+            $x = 1
+        }
+
+        else{ 
+        $servers = Import-Csv serverlist.csv
+
+            foreach($server in $servers.name){
+               Start-job -scriptblock {test-connection -Computername $server -quiet} -
+            }
+            Get-job
+ 
+        }
+      
+}
+
+#ping servers as a job using the serverl list file instead of making processes
